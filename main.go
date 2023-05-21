e52b64e63f7c85b7a8984bf21cba6328e3e400de
Amir Ahmad <4963254+Amir-Ahmad@users.noreply.github.com>
2023-05-21 22:39:19 +1000

    Print version and arch info

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
+
+var version = "0.0.0-dev"
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(runtime.GOOS, runtime.GOARCH)
+	fmt.Println("version: ", version)
 }
